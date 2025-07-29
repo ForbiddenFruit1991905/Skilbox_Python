a6eb132a4119be58f8009328004dd8d4537ee1be
Лихачёва Виктория Михайловна <29436838@s.masi.ru>
2025-07-29 18:25:39 +0300

    some new_task

--- basics_2/lessons/lesson_12/task_12.4.py
+++ basics_2/lessons/lesson_12/task_12.4.py
@@ -134,3 +134,59 @@ rocket = Aircraft()
 rocket.take_off()
 rocket.land(explosure=True)
 rocket.land()
+
+
+# Вариант от скилбокса:
+# class CanFly:
+#
+#     def __init__(self):
+#         self.altitude = 0  # метров
+#         self.velocity = 0  # км/ч
+#
+#     def take_off(self):
+#         pass
+#
+#     def fly(self):
+#         pass
+#
+#     def land_on(self):
+#         self.altitude = 0
+#         self.velocity = 0
+#
+#     def __str__(self):
+#         return '{} высота {} скорость {}'.format(
+#             self.__class__.__name__, self.altitude, self.velocity,
+#         )
+#
+#
+# class Butterfly(CanFly):
+#
+#     def take_off(self):
+#         self.altitude = 1
+#
+#     def fly(self):
+#         self.velocity = 0.1
+#
+#
+# class Aircraft(CanFly):
+#
+#     def take_off(self):
+#         self.velocity = 300
+#         self.altitude = 1000
+#
+#     def fly(self):
+#         self.velocity = 800
+#
+#
+# class Missile(CanFly):
+#
+#     def take_off(self):
+#         self.velocity = 1000
+#         self.altitude = 10000
+#
+#     def land_on(self):
+#         self.altitude = 0
+#         self.destroy_enemy_base()
+#
+#     def destroy_enemy_base(self):
+#         print('Ракета показала себя великолепно. Только упала не на ту планету (C) Вернер фон Браун')
