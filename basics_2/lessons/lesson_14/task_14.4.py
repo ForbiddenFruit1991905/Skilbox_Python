90f39a8f686ea68cfe2f2dee45162409a0a0761a
Лихачёва Виктория Михайловна <29436838@s.masi.ru>
2025-08-11 16:52:19 +0300

    some new_task

--- basics_2/lessons/lesson_14/task_14.4.py
+++ basics_2/lessons/lesson_14/task_14.4.py
@@ -41,6 +41,33 @@ def make_sandwich():
 
 make_sandwich()
 
+# Вариант от скилбокса
+
+def get_some_salad(func):
+    def wrap_that_salad(*args, **kwargs):
+        print("#помидоры#")
+        func(*args, **kwargs)
+        print("~салат~")
+
+    return wrap_that_salad
+
+
+def get_some_buns(func):
+    def wrap_that_buns(*args, **kwargs):
+        print("</----------\>")
+        func(*args, **kwargs)
+        print("<\______/>")
+
+    return wrap_that_buns
+
+
+@get_some_buns
+@get_some_salad
+def filling_burger(filler):
+    print(filler)
+
+
+filling_burger("ветчина")
 
 # Задача 2. Плагины
 # Один проект состоит из огромного количества разнообразных функций, часть из которых
@@ -49,3 +76,29 @@ make_sandwich()
 # Реализуйте специальный декоратор, который будет «регистрировать» нужные функции как
 # плагины и заносить их в соответствующий словарь.
 
+from typing import Callable, Dict
+
+
+PLUGINS: Dict[str, Callable] = dict()
+
+def register_plugins(func: Callable) -> Callable:
+    PLUGINS[func.__name__] = func
+    return func
+
+
+@register_plugins
+def say_hello(name: str) -> str:
+    return f"Hello, {name}"
+
+
+@register_plugins
+def question() -> str:
+    some_user_question = input('Введите любой вопрос:\n')
+    return some_user_question
+
+
+print(PLUGINS)
+print(say_hello('Vika'))
+print(question())
+
+print(PLUGINS['say_hello']('Vika'))
